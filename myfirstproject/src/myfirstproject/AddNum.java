9f552fd84a2265d1873202cd394ba44e8a2f9d49
ukvikpoo <poonam.27dec@gmail.com>
2022-11-14 13:23:38 +0000

    Adding new eclipse files

---
+++ myfirstproject/src/myfirstproject/AddNum.java
@@ -0,0 +1,20 @@
+package myfirstproject;
+
+public class AddNum {
+
+	public static void main(String[] args) {
+		// Add two given number and print the output
+int first;
+int second;
+int sum;
+
+first = 324;
+second = 456;
+
+sum = first + second;
+
+System.out.println("Sum of " + first + " and " +  second + " = ");
+System.out.println(sum);
+	}
+
+}
